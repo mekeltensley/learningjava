780b2c3e6d93c17c489678fe20f53477cf00e00d
Mekel Tensley <mekelintech@gmail.com>
2022-07-06 02:20:21 -0400

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        input.nextDouble();
+
+//        int  [] numbers = {2, 0, 1};
+//        String [] names = {"Ali", "Maria"};
+
+//        int [] numbers = new int[3];
+//        numbers[0] = 0;
+//        numbers[1] = 1;
+//        numbers[2] = 2;
+//        System.out.println(Arrays.toString(numbers));
+//        System.out.println(Arrays.toString(names));
+
+        double studentGPA = 3.45;
+        String studentFirstName = "Kayla";
+        String studentLastName = "Harley";
+
+        System.out.println(studentFirstName + " " +
+                studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+    }
+}
