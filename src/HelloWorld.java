68871ca1ba6624fba71f8b96be58854a3c0de9df
Mekel Tensley <mekelintech@gmail.com>
2022-07-12 13:02:46 -0400

    Using Scanner to take input from user

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 class HelloWorld {
 
@@ -16,12 +16,42 @@ class HelloWorld {
 //        System.out.println(Arrays.toString(numbers));
 //        System.out.println(Arrays.toString(names));
 
-        double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Harley";
+//        double studentGPA = 3.45;
+//        String studentFirstName = "Kayla";
+//        String studentLastName = "Harley";
+//
+//        System.out.println(studentFirstName + " " +
+//                studentLastName + " has a GPA of " + studentGPA);
+//        System.out.println("What do you want to update it to?");
+//
+//        Scanner input = new Scanner(System.in);
+//        studentGPA = input.nextDouble();
+//        System.out.println(studentFirstName + " " +
+//                studentLastName + " now has a GPA of " + studentGPA);
+            String question = "What is cookie monster's favorite snack?";
+            String choiceOne = "cookies";
+            String choiceTwo = "chocolate cookies ";
+            String choiceThree = "walnuts";
 
-        System.out.println(studentFirstName + " " +
-                studentLastName + " has a GPA of " + studentGPA);
-        System.out.println("What do you want to update it to?");
+            String correctAnswer = choiceTwo;
+
+            System.out.println(question);
+
+            System.out.println("Choose one of the following: " +
+                    choiceOne + ", " + choiceTwo +  ", or " + choiceThree);
+
+            Scanner scanner = new Scanner(System.in);
+
+            String input = scanner.next();
+
+            if (correctAnswer.equals(input)) {
+                System.out.println("Congrats! That's the correct answer");
+            } else {
+                System.out.println("You are incorrect. The correct answer is: "
+                        + correctAnswer);
+            }
+
+        }
     }
-}
+
+
